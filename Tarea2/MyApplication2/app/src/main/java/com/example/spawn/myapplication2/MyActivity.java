695b00f0b4e1d54fcec5f0839ea253e51c900baf
Fabrizio <frcavada@uc.cl>
2014-09-05 00:22:13 -0400

    Tarea 2 terminada

--- Tarea2/MyApplication2/app/src/main/java/com/example/spawn/myapplication2/MyActivity.java
+++ Tarea2/MyApplication2/app/src/main/java/com/example/spawn/myapplication2/MyActivity.java
@@ -4,16 +4,52 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
 
+    int counter = 0;
+    int result = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_my);
+    }
+    public void onClick(View view){
+        TextView textView = (TextView)findViewById(R.id.textViewResult);
+        if (counter == 0)textView.setText("");
+        counter++;
+        if (view instanceof Button){
+            Button button = (Button) view;
+            if (button.getText().equals("Solve") && counter == 4){
+                counter = 0;
+                char op = textView.getText().charAt(1);
+                int a = Integer.parseInt(textView.getText().charAt(0)+"");
+                int b = Integer.parseInt(textView.getText().charAt(2)+"");
+
+                switch (op){
+                    case '+':
+                        result = (a+b);
+                        break;
+                    case '-':
+                        result = (a - b);
+                        break;
+                    case 'X':
+                        result = (a * b);
+                        break;
+                    case '/':
+                        result = (a / b);
+                        break;
+                }
+                textView.setText(""+result);
+            }
+            else textView.append(button.getText());
+        }
     }
 
 
